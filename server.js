cecd60d25e322947335227c4055c1e6d5a574e7e
Maaz Ahmad <maaz.ahmad213@gmail.com>
2022-01-25 13:31:09 +0500

    da

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const fs = require("fs");
+const fileUpload = require("express-fileupload");
 const app = express();
+app.use(fileUpload());
 app.use(express.json());
 
 app.use(express.json({ limit: "50mb" }));
@@ -8,21 +10,39 @@ app.use(express.urlencoded({ limit: "50mb" }));
 app.use(express.static("public"));
 
 app.get("/time", (req, res) => {
-  fs.readFile("./all.json", async function (err, data) {
-    let year = await JSON.parse(data);
-    res.send(year);
+  res.download("./all.json");
+  //   fs.readFile("./all.json", async function (err, data) {
+  //     let year = await JSON.parse(data);
+  //     res.send(year);
+  //   });
+});
+
+app.post("/time", function (req, res) {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.sampleFile;
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv("./all.json", function (err) {
+    if (err) return res.status(500).send(err);
+
+    res.send("File uploaded!");
   });
 });
-app.post("/time", (req, res) => {
-  let year = req.body.year;
-  year = JSON.stringify(year);
-  fs.writeFile("./all.json", year, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send("updated");
-    }
-  });
-});
+
+// app.post("/time", (req, res) => {
+//   let year = req.body.year;
+//   year = JSON.stringify(year);
+//   fs.writeFile("./all.json", year, (err) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.send("updated");
+//     }
+//   });
+// });
 const port = process.env.PORT || 80;
 app.listen(port, console.log("listening on " + port));
