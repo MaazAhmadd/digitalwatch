cbd230a484d175af99a4abf938e7752577b8e719
Maaz Ahmad <maaz.ahmad213@gmail.com>
2022-01-25 14:02:09 +0500

    das

--- server.js
+++ server.js
@@ -18,18 +18,24 @@ app.get("/time", (req, res) => {
 });
 
 app.post("/time", function (req, res) {
+  //   console.log(req.body);
+  //   console.log(req.files);
+  if (req.body.password !== "secretOnicpass") {
+    return res.send("wrong password, you will be banned if you keep trying");
+  }
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).send("No files were uploaded.");
   }
 
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  let sampleFile = req.files.sampleFile;
+  //   let sampleFile = req.files["uploads[]"];
 
   // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv("./all.json", function (err) {
+  req.files.prayer_times.mv("./all.json", function (err) {
     if (err) return res.status(500).send(err);
 
     res.send("File uploaded!");
+    // res.redirect("/");
   });
 });
 
